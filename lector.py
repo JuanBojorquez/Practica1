2c037cb7d665e0ddf6237ee294aa5aaefcfb57c3
Andromeda <juanbojorquez05@gmail.com>
2020-02-07 00:20:19 +0000

    Modifique lector.py

--- lector.py
+++ lector.py
@@ -1,92 +1,68 @@
 #!/usr/bin/python3
 import argparse
 
-def leer_archivo (archivo):
-    #archivo = "/tmp/episodio4.txt" 
+def leer_archivo( archivo ):
   try:
-  
-    with open(archivo,"r") as fh:
-    
-      texto =fh.read()
-    
-      lista=texto.splitlines()
-    
-      texto_limpio=" ".join(lista)
-      
+      with open(archivo,"r") as fh:
+        texto = fh.read() 
+        lineas = texto.splitlines()  
+        texto_limpio = " ".join(lineas)
   except:
-    
-    texto_limpio=""
-  
+      texto_limpio=""
+      print("Error Error Error Error Error Error Error Error Error Error")
   return texto_limpio
     
-    #fc = open(archivo,"r") # r = reading: lectura 
 
-    #texto = fc.read() 
+def contar_palabras( texto ):
+    palabras = texto.split(" ") 
+    dp = dict() 
+    for palabra in palabras: 
+        p = palabra.strip(",.") 
+        if p in dp: 
+           dp[p]+= 1
+        else: 
+          dp[p] = 1
+    del(dp[""]) 
+    return dp
 
-    #lineas = texto.splitlines() 
+def imprime_diccionario(dps, minimo):
+    lista= [(k,v) for k,v in dps.items() if v >= minimo]
+    lista_ordenada = sorted(lista, key= lambda x:x[1], reverse=True)
+    for tupla in lista_ordenada:
+        print(tupla[0],"= ",tupla[1])
+    return
 
-    #texto_limpio = " ".join(lineas) 
-  
-    #return texto_limpio
-    
 def leer_stopwords(archivo):
      try:
-             with open(archivo,"r") as fh:
-                     lista=fh.readlines()
-             lista_palabras=[palabra.strip("\n") for palabra in lista
+         with open(archivo,'r') as fh:
+             lista=fh.readlines()
+         lista_palabras=[palabra.strip("\n") for palabra in lista]
+         lista_palabras[0]='a'
      except:
-             lista_palabras=[]
+         print("Excepcion")
+         lista_palabras=[]
      return set(lista_palabras)
 
-    
-def contar_palabras(texto):
-    palabras = texto.split(" ") 
+def limpia_diccionario(dp, set_stopwords):
+    ndp={}
+    for k,v in dp.items():
+        if k.lower() not in set_stopwords:
+            ndp[k]=v
+    return ndp
 
-    dp = dict() 
-
-    for palabra in palabras: 
-
-      p = palabra.strip(",.") 
-
-      if p in dp: 
-
-         dp[p]+= 1 
-
-      else: 
-
-         dp[p] = 1 
-    if "" in dp:
-    
-      del (dp[""])
-    return dp
-    
-
-    
-def imprime_diccionario(dp, minimo):
-     lista=[(k,v) for k,v in dp.items() if v>=minimo]
-     lista_ordenada=sorted(lista, key=lambda x:x[1], reverse=True)
-     #print(lista_ordenada)
-     for tupla in lista_ordenada:
-             print(tupla[0],"= ",tupla[1])
-     return
-
-
-def main(archivo, minimo):
-    ###main() recibe nombre de archivo
-    ###lo abre y cuenta las palabras repetidas
-    ###
-    texto=leer_archivo(archivo)
-    dip=contar_palabras(texto)
+def main( archivo, minimo ):
+    texto = leer_archivo( archivo )
+    dip   = contar_palabras( texto )
+    stop_words= leer_stopwords("/home/andromeda/gilberto/spanish_stopwords.txt")
+    ndp = limpia_diccionario(dip,stop_words)
     #print(dip)
-    imprime_diccionario(dip,minimo)
-    
-if __name__=="__main__":
-    #archivo="/home/andromeda/Juan/capitulo4.txt" 
-    #main(archivo) 
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-a','--archivo', dest='archivo', help='nombre de archivo',required=True)
-    parser.add_argument('-m','--minimo', dest='minimo', help='minimo de repeticiones',required=False,type=int,default=3)
+    imprime_diccionario(ndp,minimo)
+
+if __name__ == "__main__":
+    parser= argparse.ArgumentParser()
+    parser.add_argument('-a','--archivo', dest='archivo', help="nombre de archivo", required=True)
+    parser.add_argument('-m','--minimo', dest='minimo', help="minimo de repeticiones", required=False,type=int,default=3)
     args=parser.parse_args()
-    minimo=args.minimo
-    archivo=args.archivo
+    minimo= args.minimo
+    archivo= args.archivo
     main(archivo,minimo)
