233de14b79c0fb69bb46f7dc85707181e42fa7ee
Andromeda <juanbojorquez05@gmail.com>
2020-02-18 00:49:58 +0000

    Termine el archivo Cita.py

--- Cita.py
+++ Cita.py
@@ -1,13 +1,34 @@
 #!/usr/bin/python3
-import lector
 import argparse
+import lector
+import contar_palabra
 
-def contar(archivo,archivo_sw):
+def obten_cita(texto,inicio,cuenta):
+    lista=texto.split(" ")
+    longitud=len(lista)
+    if ((inicio+cuenta)<longitud):
+      lista_palabras=lista[inicio:inicio+cuenta]
+      cita=" ".join(lista_palabras)
+    else:
+      cita=""
+    return cita
+    
+def main( archivo, inicio,cuenta ):
     texto=lector.leer_archivo(archivo)
+    cita=obten_cita(texto,inicio,cuenta)
+    print("Cita: ",cita)
+    stopwords=lector.leer_stopwords("/home/andromeda/gilberto/spanish_stopwords.txt")
+    contar_palabra.contar(cita,stopwords)
     
-    
-    
-def main(archivo,archivo_sw):
-  texto=lector.leer_archivo(archivo)
-  sp=lector.leer_stopwords(archivo)
-  contar(texto,sw)
+if __name__ == "__main__":
+  parser= argparse.ArgumentParser()
+  parser.add_argument('-a','--archivo', dest='archivo', help="nombre de archivo", required=True)
+  #parser.add_argument('-s','--stopwords', dest='archivo_stopwords', help="nombre de archivo stopwords", required=False,default= "spanish_stopwords.txt")
+  parser.add_argument('-i','--inicio', dest='inicio', help="numero donde iniciara la cita", required=True,default=9,type=int)
+  parser.add_argument('-c','--cuenta', dest='cuenta', help="conteo", required=True, default= 10,type=int)
+  args=parser.parse_args()
+  archivo= args.archivo
+ # archivo_sw= args.archivo_stopwords
+  inicio=args.inicio
+  cuenta=args.cuenta
+  main(archivo,inicio,cuenta)
