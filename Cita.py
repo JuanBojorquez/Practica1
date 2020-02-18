f771388834c2db8a4a445a358daf60eb811b3e1a
Andromeda <juanbojorquez05@gmail.com>
2020-02-18 00:23:21 +0000

    Agregue el archivo Cita.py otra vezxd

---
+++ Cita.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import lector
+import argparse
+
+def contar(archivo,archivo_sw):
+    texto=lector.leer_archivo(archivo)
+    
+    
+    
+def main(archivo,archivo_sw):
+  texto=lector.leer_archivo(archivo)
+  sp=lector.leer_stopwords(archivo)
+  contar(texto,sw)
