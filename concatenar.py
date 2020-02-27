a685e7eb1c9a9154bf480a70a90cd89809399e4c
Andromeda <juanbojorquez05@gmail.com>
2020-02-27 00:34:00 +0000

    Comente el archivo concatenar.py

--- concatenar.py
+++ concatenar.py
@@ -4,12 +4,16 @@ import argparse
 import os
 
 def crear_archivo(output, contenidos):
+#Se crea un archivo que es definido al ingresar el comando con los parametros y se escriben los textos conectados.
     with open(output,'w') as f:
         for item in contenidos:
             f.write("{}\n".format(item))
             
-
 def conca_archivos(archivos_new, folder):
+'''
+Se guardan las palabras de los archivos 
+y se juntan en una misma lista
+'''
     contenidos = []
     for archivo in archivos_new:
         texto = lector.leer_archivo(os.path.join(folder,archivo))
@@ -17,6 +21,10 @@ def conca_archivos(archivos_new, folder):
     return contenidos
     
 def busqueda_archivos(folder, inicia, termina):
+'''
+El metodo os.listdir(folder) sirve para ver todos los archivos que se encuentran en el directorio.
+Depues se seleccionaran los documentos tomando en cuenta los metodos archivo.starswith y archivo.endswith
+'''
     listado_archivos = os.listdir(folder)
     archivos_new = []
     for archivo in listado_archivos:
@@ -34,7 +42,7 @@ def main(folder, inicia, termina, output):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('-f','--folder', dest='folder', help="nombre de folder", required=True,)
-    parser.add_argument('-i','--inicia', dest='inicia', help="con que inicia el archivo", required=True)
+    parser.add_argument('-i','--inicia', dest='inicia', help="con que inicia el archivo", required=False, default="")
     parser.add_argument('-t','--termina', dest='termina', help="con que termina el archivo", required=True)
     parser.add_argument('-o','--output', dest='output', help="nombre del archivo de salida", required=True)
     args=parser.parse_args()
